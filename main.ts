244b883c4db6ab043b0de94d64e6525e126e305e
Cedric dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-11-22 12:13:32 -0500

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -2,7 +2,28 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program make a 28BYJ-48 DC 5V stepper motor move
+ * Also, using the new standard of using a while (true) statement
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.showIcon(IconNames.Happy)
+let distanceToObject: number = 0
+
+// loop forever
+while (true) {
+  if (input.buttonIsPressed(Button.A) == true) {
+  // find distance from sonar
+      input.onButtonPressed(Button.A, function () {
+          basic.clearScreen()
+          distanceToObject = sonar.ping(
+              DigitalPin.P1,
+              DigitalPin.P2,
+              PingUnit.Centimeters
+          )
+          basic.showNumber(distanceToObject)
+          basic.showIcon(IconNames.Happy)
+      })
+
+  }
+}
