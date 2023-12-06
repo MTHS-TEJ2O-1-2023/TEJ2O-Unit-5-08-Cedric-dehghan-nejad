7c9563dbfa7bc3c037cba51fc406784532c32534
Cedric dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-12-06 17:45:57 -0500

    please work

--- main.ts
+++ main.ts
@@ -11,33 +11,31 @@ let distanceToObject: number = 0
 basic.showIcon(IconNames.Happy)
 
 while (true) {
-  if (input.buttonIsPressed(Button.A) == true) {
+  if (input.buttonIsPressed(Button.A) === true) {
     // find distance from sonar
-      basic.clearScreen()   
-      while (true){
-    distanceToObject = sonar.ping(
-      DigitalPin.P1,
-      DigitalPin.P2,
-      PingUnit.Centimeters
-    )
+    basic.clearScreen()  
+    while (true) {
+      distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+      )
 
       basic.showNumber(distanceToObject)
       basic.showIcon(IconNames.Happy)
-    
-    robotbit.StpCarMove(10, 48)
 
-    if (distanceToObject < 10) {
-      robotbit.StpCarMove(-10, 48)
-      robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
-      robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
-      robotbit.StpCarMove(10, 48)
-      basic.pause(500)
+     robotbit.StpCarMove(10, 48)
 
-      }else{
-      robotbit.StpCarMove(10, 48)
-
-    }
+      if (distanceToObject < 10) {
+        robotbit.StpCarMove(-10, 48)
+        robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+        robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
+        robotbit.StpCarMove(10, 48)
+        basic.pause(500)
+      } else {
+        robotbit.StpCarMove(10, 48)
+      }
       basic.pause(1000)
- }
+    }
   }
 }
